e3d561a84ca9f5959711561ee99d1c61e6bb8c31
Shishir230 <as24act@herts.ac.uk>
2025-01-05 19:33:57 +0000

    Delete Statistic test.R

--- Statistic test.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-energy<-read_csv("renewable_energy.csv")
-df<-energy
-filtered_df <- energy[energy$LOCATION%in% c('AUS', 'BEL'), ]
-print(filtered_df)
-wilcox_test_result <- wilcox.test(Value ~ LOCATION, data = filtered_df)
-
-print(wilcox_test_result)
